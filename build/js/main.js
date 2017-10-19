73a0ddc6ab14ffde385c4769e7cf244fb8a341ec
Nikita Kalachev <nikitakalachev18@gmail.com>
2017-10-19 14:03:38 +0400

    release

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,40 @@
-$(document).ready(function() {
+var isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPod|iPad/i);
+	},
+	Opera: function() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function() {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	}
+};
 
-    // Если в проекте используются встроенные js-плагины от Foundation, разкомментировать
-    // $(document).foundation();
-
-});
+$(document).ready(function(){
+	if (isMobile.any()) {
+		$('.product').click(function(){
+			$(this).toggleClass('active').siblings('.product').removeClass('active');
+			$(this).find(".alt").slideToggle(0);
+			$(this).siblings('.product').find(".alt").slideUp(0);
+			console.log ('click');
+		})
+	} else {
+		$('.product').hover(function(){
+			$('.alt').stop();
+			$(this).toggleClass('active');
+			$(this).find(".alt").slideToggle(400);
+		}, function(){
+			$(this).toggleClass('active');
+			$(this).find(".alt").slideToggle(0);
+		})
+	}
+})
